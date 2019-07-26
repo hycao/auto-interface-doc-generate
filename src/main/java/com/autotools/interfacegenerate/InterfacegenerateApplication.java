b024664086fd0fde93758ba4c5ff6870f9b0e928
hycao <hycao029@gmail.com>
2019-07-26 16:27:08 +0800

    初始化仓库

---
+++ src/main/java/com/autotools/interfacegenerate/InterfacegenerateApplication.java
@@ -0,0 +1,13 @@
+package com.autotools.interfacegenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfacegenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterfacegenerateApplication.class, args);
+    }
+
+}
